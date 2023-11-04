529e84720bb5696196ace60c71fcf9ead2d28f51
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2023-11-04 16:42:53 +0530

    String Patterns

--- src/Lec2/Pattern12.java
+++ src/Lec2/Pattern12.java
@@ -6,5 +6,29 @@ public class Pattern12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int row = 1;
+        int star = 1;
+        int space = n - 1;
+        while(row <= n){
+            int j = 1;
+            while(j <= space){
+                System.out.print("   ");
+                j++;
+            }
+            int k = 1;
+            while(k<=star) {
+                if(k%2 == 0){
+                    System.out.print("!  ");
+                }
+                else{
+                    System.out.print("*  ");
+                }
+                k++;
+            }
+            System.out.println();
+            space--;
+            star+=2;
+            row++;
+        }
     }
 }
