5751b4b15e26cfb274aafa17b208afda76f8b95d
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2023-12-03 18:48:31 +0530

    Arrays

---
+++ src/Arrays/RotateArray.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+import java.util.Arrays;
+public class RotateArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+        rotate(arr, 2);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void rotate(int[] nums, int k) {
+        k %= nums.length;
+        int n = nums.length;
+        reverseNum(nums,0,n-1);
+        reverseNum(nums,0,k-1);
+        reverseNum(nums,k,n-1);
+    }
+    public static void reverseNum(int[] nums, int start, int end) {
+        while(start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
