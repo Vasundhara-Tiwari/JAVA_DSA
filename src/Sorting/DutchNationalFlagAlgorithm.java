135fb6b08a8d5e16c344890c260ca68fc8d995b5
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2023-12-03 19:24:12 +0530

    Arrays

--- src/Sorting/DutchNationalFlagAlgorithm.java
+++ src/Sorting/DutchNationalFlagAlgorithm.java
@@ -8,22 +8,22 @@ public class DutchNationalFlagAlgorithm {
         sort(arr);
         System.out.println(Arrays.toString(arr));
     }
-    public static void sort(int[] arr){
+    public static void sort(int[] nums){
         int i = 0;  // position of 0
-        int j = arr.length-1;  // position of 2
+        int j = nums.length-1;  // position of 2
         int k = 0;  // for iterating over array
         while(k <= j){
-            if(arr[k] == 2){
-                int temp = arr[j];
-                arr[j] = arr[k];
-                arr[k] = temp;
+            if(nums[k] == 2){
+                int temp = nums[j];
+                nums[j] = nums[k];
+                nums[k] = temp;
                 j--;
-            } else if(arr[k] == 1){
+            } else if(nums[k] == 1){
                 k++;
             } else{
-                int temp = arr[i];
-                arr[i] = arr[k];
-                arr[k] = temp;
+                int temp = nums[i];
+                nums[i] = nums[k];
+                nums[k] = temp;
                 i++;
                 k++;
             }
