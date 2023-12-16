eb7ec767484c3116ffc696d8ef41b1679b5d7a9c
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2023-12-16 17:18:29 +0530

    2D-Array

---
+++ src/TwoDimensionalArrays/TwoDArrayDeclaration.java
@@ -0,0 +1,18 @@
+package TwoDimensionalArrays;
+
+import java.util.Scanner;
+
+public class TwoDArrayDeclaration {
+    public static void main(String[] args) {
+        int[][] arr = {{1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}};
+        display(arr);
+    }
+    public static void display(int[][] arr){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[0].length; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
