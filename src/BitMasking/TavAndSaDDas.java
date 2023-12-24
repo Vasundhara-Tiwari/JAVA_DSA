dfe31f2b039421d45a3887012a1b889ffaa46601
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2023-12-24 19:12:29 +0530

    CP

--- src/BitMasking/TavAndSaDDas.java
+++ src/BitMasking/TavAndSaDDas.java
@@ -8,11 +8,19 @@ public class TavAndSaDDas {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String s = scanner.next();
-
+        // count total numbers 1 less in length than length of number
+        System.out.println(index(s));
     }
     public static int index(String s){
         int n = s.length();
         int count = (1 << n) -2;    // Using formula 2^n - 2
-        return count;
+        int position = 0;
+        for(int i = s.length() - 1; i >= 0; i--){
+            if(s.charAt(i) == '7'){
+                count += 1 << position;
+            }
+            position++;
+        }
+        return count+1;
     }
 }
