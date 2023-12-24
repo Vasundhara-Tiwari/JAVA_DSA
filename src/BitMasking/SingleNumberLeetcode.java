8ca93fa9113254bbd5598d3550a41f09dde1544a
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2023-12-24 18:07:27 +0530

    Bit Masking

---
+++ src/BitMasking/SingleNumberLeetcode.java
@@ -0,0 +1,15 @@
+package BitMasking;
+
+public class SingleNumberLeetcode {
+    public static void main(String[] args) {
+        int[] nums = {4,1,2,1,2};
+        System.out.println(singleNumber(nums));
+    }
+    public static int singleNumber(int[] nums) {
+        int result = 0;
+        for(int i = 0; i < nums.length; i++){
+            result = result ^ nums[i];
+        }
+        return result;
+    }
+}
