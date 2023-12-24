4afa47cb74fb0b4db113d99200cdfe210f2df4c4
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2023-12-24 17:56:36 +0530

    Bit Masking

--- src/BitMasking/CountSetBits.java
+++ src/BitMasking/CountSetBits.java
@@ -4,11 +4,17 @@ public class CountSetBits {
     public static void main(String[] args) {
         int n = 1879;
         int count = 0;
+        // method 1
+//        while(n > 0){
+//            if((n&1) != 0){
+//                count++;
+//            }
+//            n >>=1;
+//        }
+        //method 2
         while(n > 0){
-            if((n&1) != 0){
-                count++;
-            }
-            n >>=1;
+            count++;
+            n = n & n-1;
         }
         System.out.println(count);
     }
