26c8bd3d4e6d06459ac81adc3e71b3c722b513db
Vasundhara-Tiwari <vasundhara.tiwari.321@gmail.com>
2024-02-10 18:07:51 +0530

    Stack

---
+++ src/Stack/StackImplementation.java
@@ -0,0 +1,51 @@
+package Stack;
+
+public class StackImplementation {
+    private int[] stack;
+    private int index = 0;
+
+    public StackImplementation(){
+        stack = new int[5];
+    }
+
+    public StackImplementation(int n){
+        stack = new int[n];
+    }
+
+    public void push(int element){
+        try{
+            if(stack.length <= index){
+                throw new Exception("Stack Overflow!!!");
+            }
+            stack[index] = element;
+            index++;
+        } catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public int pop(){
+        try{
+            if(index < 0){
+                throw new Exception("Stack Underflow!!!");
+            }
+            int element = stack[index-1];
+            index--;
+            return element;
+        } catch (Exception e){
+            System.out.println(e);
+            return -1;
+        }
+    }
+
+    public int peek(){
+        return stack[index-1];
+    }
+    public int getSize(){
+        return index;
+    }
+
+    public boolean isEmpty(){
+        return index == 1;
+    }
+}
---
+++ src/Stack/StackImplementationClient.java
@@ -0,0 +1,16 @@
+package Stack;
+
+public class StackImplementationClient {
+    public static void main(String[] args) {
+        StackImplementation stack = new StackImplementation(4);
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+        System.out.println(stack.peek());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+    }
+}
