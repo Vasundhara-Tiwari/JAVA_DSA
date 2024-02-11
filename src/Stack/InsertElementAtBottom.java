e54b19c7a3ef36c2d5cd7a0ada29c95bc52d8a7d
Vasundhara-Tiwari <vasundhara.tiwari.321@gmail.com>
2024-02-11 17:17:52 +0530

    stack

--- src/Stack/InsertElementAtBottom.java
+++ src/Stack/InsertElementAtBottom.java
@@ -11,16 +11,16 @@ public class InsertElementAtBottom {
         stack.push(40);
         stack.push(50);
         System.out.println(stack);
-        add(stack, -7);
+        insertAtBottom(stack, -7);
         System.out.println(stack);
     }
-    public static void add(Stack<Integer> stack, int item){
+    public static void insertAtBottom(Stack<Integer> stack, int item){
         if(stack.isEmpty()){
             stack.push(item);
             return;
         }
         int ele = stack.pop();
-        add(stack, item);
+        insertAtBottom(stack, item);
         stack.push(ele);
     }
 }
