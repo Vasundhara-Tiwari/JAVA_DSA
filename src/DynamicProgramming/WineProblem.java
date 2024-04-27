7513e5b1a53bd52e517580d7e8b8693190422ff5
Vasundhara-Tiwari <vasundhara.tiwari.321@gmail.com>
2024-04-27 19:08:11 +0530

    Dynamic Programming

--- src/DynamicProgramming/WineProblem.java
+++ src/DynamicProgramming/WineProblem.java
@@ -3,14 +3,18 @@ package DynamicProgramming;
 public class WineProblem {
     public static void main(String[] args) {
         int[] winePrices = {2, 3, 5, 1, 4};
-        System.out.println(maxProfit(winePrices, 0, winePrices.length-1, 1));
+        int[][] dp = new int[winePrices.length][winePrices.length];
+        System.out.println(maxProfit(winePrices, 0, winePrices.length-1, 1, dp));
     }
-    public static  int maxProfit(int[] wine, int si, int ei, int year){
+    public static  int maxProfit(int[] wine, int si, int ei, int year, int[][] dp){
         if(si > ei){
             return 0;
         }
-        int fs = wine[si] * year + maxProfit(wine, si+1, ei, year+1);
-        int ls = wine[ei] * year + maxProfit(wine, si, ei-1, year+1);
-        return Math.max(fs, ls);
+        if(dp[si][ei] != 0){
+            return dp[si][ei];
+        }
+        int fs = wine[si] * year + maxProfit(wine, si+1, ei, year+1, dp);
+        int ls = wine[ei] * year + maxProfit(wine, si, ei-1, year+1, dp);
+        return dp[si][ei] = Math.max(fs, ls);
     }
 }
